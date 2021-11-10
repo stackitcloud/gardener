e5934c42544a2f06c0da879a621a51d6cb0e64b4
Manuel Ganter <manuel.ganter@hotmail.de>
2021-11-10 14:10:56 +0100

    v1.24.1-ske-17: removed proxyConfig settings

--- hacky.sh
+++ hacky.sh
@@ -94,8 +94,9 @@
 # v1.24.0-ske-14: ReversedVPN fix for vpn seed server
 # v1.24.0-ske-15: Real ReversedVPN fix for vpn seed server
 # v1.24.0-ske-16: Very Real ReversedVPN fix for vpn seed server
+# v1.24.1-ske-17: removed proxyConfig settings
 
-EFFECTIVE_VERSION=v1.24.0-ske-16
+EFFECTIVE_VERSION=v1.24.0-ske-17
 REGISTRY=registry.ske.eu01.stackit.cloud/gardener-ds
 APISERVER_IMAGE_REPOSITORY=$REGISTRY/apiserver
 CONROLLER_MANAGER_IMAGE_REPOSITORY=$REGISTRY/controller-manager
