be097d61cf5ae7a52a621ff50d5c2f0da462b52c
Tim Ebert <timebertt@gmail.com>
2023-02-07 10:36:54 +0100

    Increase timeout for cache informer sync readiness check

--- pkg/healthz/informer.go
+++ pkg/healthz/informer.go
@@ -30,12 +30,12 @@ type cacheSyncWaiter interface {
 // NewCacheSyncHealthz returns a new healthz.Checker that will pass only if all informers in the given cacheSyncWaiter sync.
 func NewCacheSyncHealthz(cacheSyncWaiter cacheSyncWaiter) healthz.Checker {
 	return func(_ *http.Request) error {
-		// cache.Cache.WaitForCacheSync is racy for closed context, so use context with 1ms timeout instead.
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
+		// limit waiting for cache sync to 100ms, using a shorter timeout might be problematic in throttled containers
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 		defer cancel()
 
 		if !cacheSyncWaiter.WaitForCacheSync(ctx) {
-			return fmt.Errorf("informers not synced yet")
+			return fmt.Errorf("informers not synced yet: %w", ctx.Err())
 		}
 		return nil
 	}
