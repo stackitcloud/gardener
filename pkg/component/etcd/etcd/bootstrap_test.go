46aeffb29989f36e238a78d50f0aed05d04a3bab
Marcel Boehm <marcel.boehm@inovex.de>
2024-06-12 11:45:10 +0200

    Only specify args in etcd-druid

--- pkg/component/etcd/etcd/bootstrap_test.go
+++ pkg/component/etcd/etcd/bootstrap_test.go
@@ -360,8 +360,7 @@ spec:
         networking.gardener.cloud/to-runtime-apiserver: allowed
     spec:
       containers:
-      - command:
-        - /etcd-druid
+      - args:
         - --enable-leader-election=true
         - --ignore-operation-annotation=false
         - --disable-etcd-serviceaccount-automount=true
@@ -423,8 +422,7 @@ spec:
         networking.gardener.cloud/to-runtime-apiserver: allowed
     spec:
       containers:
-      - command:
-        - /etcd-druid
+      - args:
         - --enable-leader-election=true
         - --ignore-operation-annotation=false
         - --disable-etcd-serviceaccount-automount=true
