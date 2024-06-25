97ae87a4804d0d79deddb67cf2a5602b093860c5
Tim Ebert <timebertt@gmail.com>
2024-06-25 10:15:12 +0200

    add custom configuration for `Garden`

--- pkg/component/gardener/apiserver/deployment.go
+++ pkg/component/gardener/apiserver/deployment.go
@@ -100,6 +100,8 @@ func (g *gardenerAPIServer) deployment(
 							"--log-level=" + g.values.LogLevel,
 							"--log-format=" + g.values.LogFormat,
 							fmt.Sprintf("--secure-port=%d", port),
+							// TODO: replace this hardcoded configuration with proper fields in the Garden API
+							"--shoot-admin-kubeconfig-max-expiration=4320h", // 6 months
 						},
 						Ports: []corev1.ContainerPort{{
 							Name:          "https",
