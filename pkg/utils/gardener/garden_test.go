57106b6c06e8477460c0a1a2aa6f0d8d00e6d1ce
Duciwuci <duciwuci@gmail.com>
2025-03-21 13:33:42 +0100

    Remove Extension Validation for Provider in ETCD-Backup
    With Gardener v1.114+ the operator extensions are a required resource for the upgrade.
    In our use case, we need to deactivate this validation since this is a breaking change.
    We need to do a backup-s3 migration.
    
    Drop this commit, after the migration of the extension was performed.

--- pkg/utils/gardener/garden_test.go
+++ pkg/utils/gardener/garden_test.go
@@ -396,9 +396,7 @@ var _ = Describe("Garden", func() {
 				},
 			}
 
-			Expect(ComputeRequiredExtensionsForGarden(garden).UnsortedList()).To(ConsistOf(
-				"BackupBucket/local-infrastructure",
-			))
+			Expect(ComputeRequiredExtensionsForGarden(garden).UnsortedList()).To(BeEmpty())
 		})
 
 		It("should return required DNSRecord extension types", func() {
@@ -446,7 +444,6 @@ var _ = Describe("Garden", func() {
 			}
 
 			Expect(ComputeRequiredExtensionsForGarden(garden).UnsortedList()).To(ConsistOf(
-				"BackupBucket/local-infrastructure",
 				"DNSRecord/local-dns",
 				"Extension/local-extension-1",
 				"Extension/local-extension-2",
