b824effeceb4a441dd81ae8859db72c42b248675
Duciwuci <duciwuci@gmail.com>
2025-03-21 13:33:42 +0100

    Remove Extension Validation for Provider in ETCD-Backup
    With Gardener v1.114+ the operator extensions are a required resource for the upgrade.
    In our use case, we need to deactivate this validation since this is a breaking change.
    We need to do a backup-s3 migration.
    
    Drop this commit, after the migration of the extension was performed.

--- pkg/utils/gardener/garden.go
+++ pkg/utils/gardener/garden.go
@@ -374,9 +374,9 @@ func IsServedByKubeAPIServer(resource string) bool {
 func ComputeRequiredExtensionsForGarden(garden *operatorv1alpha1.Garden) sets.Set[string] {
 	requiredExtensions := sets.New[string]()
 
-	if helper.GetETCDMainBackup(garden) != nil {
-		requiredExtensions.Insert(ExtensionsID(extensionsv1alpha1.BackupBucketResource, garden.Spec.VirtualCluster.ETCD.Main.Backup.Provider))
-	}
+	// if helper.GetETCDMainBackup(garden) != nil {
+	// 	requiredExtensions.Insert(ExtensionsID(extensionsv1alpha1.BackupBucketResource, garden.Spec.VirtualCluster.ETCD.Main.Backup.Provider))
+	// }
 
 	for _, provider := range helper.GetDNSProviders(garden) {
 		requiredExtensions.Insert(ExtensionsID(extensionsv1alpha1.DNSRecordResource, provider.Type))
